e9a94784751e9c6e43c56b6732d5e5d992ca9c56
Jinhyuk Jeon <okjinhyuk@snu.ac.kr>
2018-12-10 20:42:58 +0900

    dd

---
+++ my/tfbasic1.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+
+tf.enable_eager_execution()
+
+print(tf.add(1, 2))
+print(tf.add([1, 2], [3, 4]))
+print(tf.square(5))
+print(tf.reduce_sum([1, 2, 3]))
+print(tf.encode_base64("hello world"))
+
+# Operator overloading is also supported
+print(tf.square(2) + tf.square(3))
